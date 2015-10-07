38678aa64e35ab4dce153f9218dc65b561513628
13019 <632268588@qq.com>
2015-10-07 21:07:00 +0800

    10.7night
    10月7号晚做的第一个版本

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php
+//$localhost=SAE_MYSQL_HOST_M;
+//$port=SAE_MYSQL_PORT;
+//$hostuser=SAE_MYSQL_USER;
+//$pass=SAE_MYSQL_PASS;
+$localhost=localhost;
+$port=3306;
+$hostuser=root;
+$pass=root;
+//
+$dbname=13019;
+//$tablename=persons;
+$conn = @mysql_connect($localhost.':'.$port,$hostuser,$pass) or die("coulde not connect");
+mysql_select_db("$dbname",$conn);
+mysql_query("set names 'utf8'"); 
+?>
