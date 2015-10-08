b80409759cbd222b08fa4ed7cd4df820ddf92d33
13019 <632268588@qq.com>
2015-10-08 13:29:40 +0800

    Revert "10.8中午"
    This reverts commit ad2d2eaa0309163166b0fb57d91295a9adf1d431.

--- register_do.php
+++ register_do.php
@@ -1,32 +1,9 @@
 <?php
 include_once("conn.php");
 $user_name=$_POST["user_name"];
-$user_password=md5($_POST["user_password"]);
-$user_regtime=time();
-
-//服务器端验证邮箱
-if($user_name){
-	 $pattern = "/^([0-9A-Za-z\\-_\\.]+)@([0-9a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?)$/i";
-	if(preg_match( $pattern, $email_address)){
-		
-		}else{
-			echo"请正确输入邮箱";
-			} 
-	}
+$user_password=$_POST["user_password"];
 
 
-//验证用户名是否存在，不存在就插入数据；
-if(isset($_POST["submit"])){
-	$sql="select user from".$dbname."where user_name='".$_POST['user_name']."'";
-	$result=mysql_query($sql);
-	if(!empty($result)){
-		echo"用户名已经存在";
-		}
-	}else{
-		$sql="inset into user(user_id,user_name,user_password,$user_regtime) value('','$user_name','$user_password','$user_regtime')";
-		mysql_query($sql);
-		}
-		
-		
-		
+
+
 ?>
