3e7e09c251f73bcd772fb2dd371621aef016c061
13019 <632268588@qq.com>
2015-10-10 23:02:51 +0800

    10.10晚

--- checkcode.php
+++ checkcode.php
@@ -1,3 +1,12 @@
 <?php
 //验证码
+session_start();
+//随机4个字符，不含0和o；
+$char="abcdefghigklmnpqrstuvwxyz123456789";
+$char_len=strlen($char)-1;
+for($i=0;$i<4;$i++){
+	$code.=$char[rand(0,$char_len)];
+	
+	}
+echo $code;
 ?>
