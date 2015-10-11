2a3a85df72874f366bb84a0538d1529c2657f79c
13019 <632268588@qq.com>
2015-10-11 14:24:35 +0800

    10.11中午

--- checkcode.php
+++ checkcode.php
@@ -8,5 +8,23 @@ for($i=0;$i<4;$i++){
 	$code.=$char[rand(0,$char_len)];
 	
 	}
-echo $code;
+//创建图片
+$im=@imagecreatetruecolor(60,25)or die("Cannot Initialize new GD image stream");//创建图片
+$bg_color=imagecolorallocate($im,rand(0,10),rand(0,10),rand(0,10));//背景颜色
+$text_color=imagecolorallocate($im,rand(150,255),rand(150,255),rand(150,255));//文字颜色
+
+for($i=0;$i<rand(10,30);$i++){//添加干扰线条
+	
+   $text_color2=imagecolorallocate($im,rand(100,200),rand(100,200),rand(100,200));
+   imageline($im,rand(0,30),rand(0,10),rand(35,60),rand(15,25),$text_color2);
+}
+for($i=0;$i<rand(100,300);$i++){//添加干扰像素
+	 $text_color3=imagecolorallocate($im,rand(100,200),rand(100,200),rand(100,200));
+	 imagesetpixel($im,rand(0,60),rand(0,25),$text_color3);
+	}
+imagestring($im,6,0,0,$code,$text_color);//将文字写在图片上
+$_SESSION["cheeckcode"]=$code;//写入session
+header("Content-type: image/png");
+imagepng($im);
+imagedestroy();//释放内存
 ?>
