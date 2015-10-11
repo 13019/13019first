c9159d827a08b74d5390ee7755688dc779364ca2
13019 <632268588@qq.com>
2015-10-11 22:37:31 +0800

    10.11晚
    验证码完毕

--- checkcode.php
+++ checkcode.php
@@ -9,22 +9,22 @@ for($i=0;$i<4;$i++){
 	
 	}
 //创建图片
-$im=@imagecreatetruecolor(60,25)or die("Cannot Initialize new GD image stream");//创建图片
+$im=@imagecreatetruecolor(70,30)or die("Cannot Initialize new GD image stream");//创建图片
 $bg_color=imagecolorallocate($im,rand(0,10),rand(0,10),rand(0,10));//背景颜色
 $text_color=imagecolorallocate($im,rand(150,255),rand(150,255),rand(150,255));//文字颜色
 
-for($i=0;$i<rand(10,30);$i++){//添加干扰线条
+for($i=0;$i<rand(0,50);$i++){//添加干扰线条
 	
    $text_color2=imagecolorallocate($im,rand(100,200),rand(100,200),rand(100,200));
-   imageline($im,rand(0,30),rand(0,10),rand(35,60),rand(15,25),$text_color2);
+   imageline($im,rand(0,70),rand(0,30),rand(0,70),rand(0,30),$text_color2);
 }
-for($i=0;$i<rand(100,300);$i++){//添加干扰像素
+for($i=0;$i<rand(100,600);$i++){//添加干扰像素
 	 $text_color3=imagecolorallocate($im,rand(100,200),rand(100,200),rand(100,200));
-	 imagesetpixel($im,rand(0,60),rand(0,25),$text_color3);
+	 imagesetpixel($im,rand(0,70),rand(0,30),$text_color3);
 	}
-imagestring($im,6,0,0,$code,$text_color);//将文字写在图片上
+imagestring($im,6,rand(0,20),rand(0,5),$code,$text_color);//将文字写在图片上
 $_SESSION["cheeckcode"]=$code;//写入session
 header("Content-type: image/png");
 imagepng($im);
-imagedestroy();//释放内存
+//imagedestroy();//释放内存
 ?>
--- register_do.php
+++ register_do.php
@@ -1,9 +1,24 @@
 <?php
+session_start();
 include_once("conn.php");
 $user_name=$_POST["user_name"];
 $user_password=md5($_POST["user_password"]);
 $user_repassword=md5($_POST["user_repassword"]);
+$user_code=$_POST["user_code"];
 $user_regtime=time();
+//验证验证码
+if($_SESSION["cheeckcode"]!=$user_code){
+	echo"验证码错误，请重新输入";
+	return false;
+	}
+if(empty($usr_name)){
+	echo"请输入用户名";
+	return false;
+		}
+if(empty($user_password)){
+	echo"请输入密码";
+	return false;
+		}
  //echo"$user_name";
  //echo"$user_password";
 //服务器端验证邮箱
@@ -13,16 +28,19 @@ if($user_name){
 		
 		}else{
 			echo"请正确输入邮箱";
+			return false;
 			} 
 	}
 //验证密码长度
 $len_user_password=strlen($user_password);
 if($len_user_password<6){
 	echo"密码长度不能小于6";
+	return false;
 	}
 //验证两次密码
 if($user_password<>$user_repassword){
 	echo"两次密码不一样，请从新输入";
+	return false;
 	}
 //验证用户名是否存在，不存在就插入数据；
 if(isset($_POST["submit"])){
@@ -30,6 +48,7 @@ if(isset($_POST["submit"])){
 	$result=mysql_query($sql);
 	if(!empty($result)){
 		echo"用户名已经存在";
+		return false;
 		}
 	else{
 		$sql="insert into user (user_name,user_password,user_regtime) values ('$user_name','$user_password','$user_regtime')";
