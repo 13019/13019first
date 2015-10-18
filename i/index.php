ac3f0798e36eccf08d3c4d5cf105857f6f4989bb
13019 <632268588@qq.com>
2015-10-18 13:39:07 +0800

    10.18中午

--- i/index.php
+++ i/index.php
@@ -4,11 +4,14 @@ include_once("../login_in.php");
 function list_user_thing($user_id){
   $sql="select * from user_something where `user_id` = '$user_id'";
   $query= mysql_query($sql);
-
  while($row=mysql_fetch_array($query)){
-   echo $row[user_thing_name];
+?>
+<div class="user_thing">
+    <div class="user_thing_list"><a href="index.php?user_id=<?php echo  $row[user_id];?>?user_thing=<?php echo  $row[user_thing_id];?>"><?php echo $row[user_thing_name];?> </a></div>
+</div>
+<?php
+
  }
 }
-
 list_user_thing($_SESSION["user_id"]);
 ?>
